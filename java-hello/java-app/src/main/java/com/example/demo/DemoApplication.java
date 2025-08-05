db2cbc3bf61f17c3578a1d6e71fb03cb2181f81f
Ortega Martinez <C95059698@adxuser.com>
2025-08-05 09:48:30 -0400

    adding new comment

--- java-hello/java-app/src/main/java/com/example/demo/DemoApplication.java
+++ java-hello/java-app/src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
+
+        // Colores ANSI
+        String red = "\u001B[31m";
+        String green = "\u001B[32m";
+        String blue = "\u001B[34m";
+        String reset = "\u001B[0m";
+
+        System.out.println(green + "✅ Aplicación iniciada correctamente!" + reset);
+        System.out.println(blue + "🌐 URL: http://localhost:8080" + reset);
+        System.out.println(red + "❤️ Gracias por usar nuestra app!" + reset);
     }
 }
--- java-hello/java-app/src/main/java/com/example/demo/controller/HelloController.java
+++ java-hello/java-app/src/main/java/com/example/demo/controller/HelloController.java
@@ -8,6 +8,15 @@ public class HelloController {
 
     @GetMapping("/")
     public String hello() {
-        return "Hello World!";
+        // return "Hello World! Bob";
+        return """
+                <html>
+                  <body style='text-align:center; font-family:Arial;'>
+                    <h1 style='color:green'>✅ Hello RTX Team</h1>
+                    <img src='/images/logo.png' width='200'/>
+                    <p>We are on Azure</p>
+                  </body>
+                </html>
+                """;
     }
 }
